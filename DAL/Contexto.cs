1bd719fbde71f6a7ffe7b41dfeb5796b3ba96c48
panda <panda@DESKTOP-BLRQCTT>
2020-06-15 11:06:02 -1100

    ventana creada

---
+++ DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnaMPrimerParcial.DAL
+{
+    class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("");
+
+
+        }
+    }
+}
